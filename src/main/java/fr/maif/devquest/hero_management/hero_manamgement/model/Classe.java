fa4aecf53b4036d834d9c855b29950478441afb4
Benjamin Cavy <benjamin.cavy@gmail.com>
2025-06-05 03:37:56 +0200

    WIP

--- src/main/java/fr/maif/devquest/hero_management/hero_manamgement/model/Classe.java
+++ src/main/java/fr/maif/devquest/hero_management/hero_manamgement/model/Classe.java
@@ -1,7 +1,5 @@
 package fr.maif.devquest.hero_management.hero_manamgement.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Classe {
     archer,
